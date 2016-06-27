566e8a69c03eb164db4c5d8e578b0c06580372d4
Zack <izaac.corbett@gmail.com>
2016-06-26 23:08:38 -0500

    First Commit
    No personal login credz. Just Source.

---
+++ SendSMS.py
@@ -0,0 +1,46 @@
+from googlevoice import Voice
+from googlevoice.util import input
+from googlevoice import Voice,util
+from time import sleep
+
+voice = Voice()
+
+try:
+	with open('obfuscateMe.txt', 'r') as obfuscateMe:
+		for lineA in obfuscateMe:
+			NeedsEncryption = lineA
+    
+	with open('obfuscateEncryptionMe.txt', 'r') as obEncryptionMe:
+		for lineB in obEncryptionMe:
+			NeedsMoreEncryption = lineB
+    
+	with open('SendTo.txt', 'r') as sendToNumba:
+		for lineC in sendToNumba:
+			sendTo = lineC
+
+finally:
+	obfuscateMe.close()
+	obEncryptionMe.close()
+	sendToNumba.close()
+
+voice.login(NeedsEncryption, NeedsMoreEncryption)
+
+while True:
+	folder = voice.search('NuffinNuffin')
+	if len(folder) == 1:
+		print 'hellowzworld'
+		message = 'HellowzWorld' #input('Message text: ')
+		voice.send_sms(sendTo,message)
+		sleep(5)
+	else:
+		print 'nuffin here to see'
+		sleep(5)
+
+"""
+voice = Voice()
+voice.login()
+
+for message in voice.sms().messages:
+    if message.isRead:
+        message.delete()
+"""
