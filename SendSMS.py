c79a721e3ab551b52de07d3cf764b7ec86155c9f
Zack <izaac.corbett@gmail.com>
2016-06-27 22:21:52 -0500

    added API key for wunderground, rm sms if responded to
    Added wunderground API scraper for weather, humidity, percip, etc...
    activated by receiving sms with "weather" keyword
    
    Added func for deleting received messages in inbox if they've been
    replied to.

--- SendSMS.py
+++ SendSMS.py
@@ -1,5 +1,7 @@
 from googlevoice import Voice,util
 from time import sleep
+import urllib2
+import json
 
 voice = Voice()
 
@@ -15,28 +17,53 @@ try:
 	with open('SendTo.txt', 'r') as sendToNumba:
 		for lineC in sendToNumba:
 			sendTo = lineC
+	with open('APIKey.txt', 'r') as APIKeyText:
+		for lineD in APIKeyText:
+			APIKey = lineD
 
 finally:
 	obfuscateMe.close()
 	obEncryptionMe.close()
 	sendToNumba.close()
 
+def getTemp():
+	f = urllib2.urlopen('http://api.wunderground.com/api/%s/geolookup/conditions/q/MN/Minneapolis.json') % lineD
+	json_string = f.read()
+	parsed_json = json.loads(json_string)
+	location = parsed_json['location']['city']
+	temp_f = parsed_json['current_observation']['temp_f']
+	condish = parsed_json['current_observation']['relative_humidity']
+	weather = parsed_json['current_observation']['weather']
+	#condition = parsed_json['conditions']
+	#print condition
+	return "%s:\nTemp: %s*F\nHumidity: %s\nWeather: %s" % (location, temp_f, condish, weather)
+	#return location, temp_f
+	f.close()
+
+def markAsRead():
+	while True :
+		folder = voice.search('is:unread')
+		if folder.totalSize <= 0:
+			break
+			util.print_(folder.totalSize)
+		for message in folder.messages:
+			util.print_(message)
+			message.delete(1)
+
+def deleteReadMessages():
+	for message in voice.sms().messages:
+	    if message.isRead:
+	        message.delete()
+
 voice.login(NeedsEncryption, NeedsMoreEncryption)
 
 while True:
-	folder = voice.search('NuffinNuffin')
+	folder = voice.search('Weather')
 	if len(folder) == 1:
-		print 'hellowzworld'
-		message = 'HellowzWorld'
+		message = str(getTemp())
 		voice.send_sms(sendTo,message)
-		sleep(5)
-		continue
+		markAsRead()
+		deleteReadMessages()
 	else:
-		print 'nuffin here to see'
+		print 'Nuffin to see here'
 		sleep(5)
-
-"""
-for message in voice.sms().messages:
-    if message.isRead:
-        message.delete()
-"""
