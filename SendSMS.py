e8703b2a18aa204b8b47cfa9ee39025647ea2bda
Zack <izaac.corbett@gmail.com>
2016-06-30 21:57:29 -0500

    housekeeping. Added AES encrypt func. Still need to isolate cleartext files from cipher from __main__
    Added an AES encryption function for phone number, log in credz, API
    key, etc... Still need to isolate cipher from cleartext files. Which
    would both be seperate from the main script. This would allow me to edit
    and upload straight to GitHub w/o running risk of exposing important
    info.

--- SendSMS.py
+++ SendSMS.py
@@ -3,7 +3,22 @@ from time import sleep
 import urllib2
 import json
 import os
-import uuid
+from Crypto.Cipher import AES
+import base64
+import binascii
+
+def Cipher(encryptme):
+	BLOCK_SIZE = 32
+	PADDING = '{'
+	pad = lambda s: s + (BLOCK_SIZE - len(s) % BLOCK_SIZE) * PADDING
+	EncodeAES = lambda c, s: base64.b64encode(c.encrypt(pad(s)))
+	DecodeAES = lambda c, e: c.decrypt(base64.b64decode(e)).rstrip(PADDING)
+	secret = os.urandom(BLOCK_SIZE)
+	cipher = AES.new(secret)
+
+	encoded = EncodeAES(cipher, encryptme)
+	decoded = DecodeAES(cipher, encoded)
+	return str(decoded)
 
 voice = Voice()
 
@@ -11,22 +26,29 @@ try:
 	with open('obfuscateMe.txt', 'r') as obfuscateMe:
 		for lineA in obfuscateMe:
 			NeedsEncryption = lineA
-    
+			Cipher(NeedsEncryption)
+
 	with open('obfuscateEncryptionMe.txt', 'r') as obEncryptionMe:
 		for lineB in obEncryptionMe:
 			NeedsMoreEncryption = lineB
+			Cipher(NeedsMoreEncryption)
     
 	with open('SendTo.txt', 'r') as sendToNumba:
 		for lineC in sendToNumba:
-			sendTo = lineC
+			SendTo = lineC
+			Cipher(SendTo)
+
 	with open('APIKey.txt', 'r') as APIKeyText:
 		for lineD in APIKeyText:
 			APIKey = lineD
+			Cipher(APIKey)
 
 finally:
 	obfuscateMe.close()
 	obEncryptionMe.close()
 	sendToNumba.close()
+
+voice.login(NeedsEncryption, NeedsMoreEncryption)
 
 def getTemp():
 	f = urllib2.urlopen('http://api.wunderground.com/api/'+ lineD +'/geolookup/conditions/q/MN/Minneapolis.json')
@@ -57,36 +79,33 @@ def deleteReadMessages():
 def newLogIn():
 	return 'New Log In'
 
-def Shutdown():
-	timeStampUUID = uuid.uuid1() #gwmi win32_bios | fl SerialNumber
-	return 'Shutting down:\n' + str(timeStampUUID)
-
-voice.login(NeedsEncryption, NeedsMoreEncryption)
+def Shutdown(): #gwmi win32_bios | fl SerialNumber
+	return 'Shutting down:\n'
 
 while True:
+	#CommandList = ['Weather', "Logged", 'Shutdown', 'Reboot']
 	WeatherCommand = voice.search('Weather')
 	LoggedCommand = voice.search('Logged')
 	ShutDownCommand = voice.search('Shutdown')
 	RebootCommand = voice.search('Reboot')
+	
 	if len(WeatherCommand) == 1:
 		message = str(getTemp())
-		voice.send_sms(sendTo, message)
+		voice.send_sms(SendTo, message)
 		markAsRead()
 		deleteReadMessages()
 		print "Weather SMS Sent"
 	elif len(LoggedCommand) == 1:
 		message = str(newLogIn())
-		voice.send_sms(sendTo, message)
+		voice.send_sms(SendTo, message)
 		markAsRead()
 		deleteReadMessages()
 	elif len(ShutDownCommand) == 1:
 		message = str(Shutdown())
-		voice.send_sms(sendTo, message)
+		voice.send_sms(SendTo, message)
 		markAsRead()
 		deleteReadMessages()
-		os.system('shutdown -s -t 0 /f')
+		#os.system('shutdown -s -t 0 /f')
 	else:
 		print 'Nuffin to see here'
 		sleep(5)
-
-computerLoggedInOrIsOn()
