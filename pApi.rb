ed055e71b01dcc32d486967bf754905f04aec3a6
Izaac <izaac.kirscht@gmail.com>
2017-05-21 10:56:09 -0500

    Update pApi.rb

--- pApi.rb
+++ pApi.rb
@@ -1,16 +1,22 @@
 require 'gmail'
 
-username = 'email@gmail.com'
-password = 'password'
-targetAddress = 'phonenumber@mymetropcs.com'
-messageSubject = 'what to say'
+credentials = []
+File.readlines('./loginCreds.txt').each do |line|
+    credentials << line
+end
+
+username = credentials[0]
+password = credentials[1]
+phoneNumber = credentials[2]
+
+bodyText = 'what to say'
 withKeyword = 'Jfjcbbd'
 
-def sendEmail(bodyText, targetAddress, username, password)
+def sendEmail(bodyText, phoneNumber, username, password)
     Gmail.connect(username, password) do |gmail|
   
         gmail.deliver do
-            to targetAddress
+            to phoneNumber
             #subject messageSubject
 
             text_part do
@@ -22,23 +28,23 @@ def sendEmail(bodyText, targetAddress, username, password)
                 body bodyText #'<p>Text of <em>html</em> message.</p>''
             end
             #add_file '\path\to\image.png'
-
         end
     end
+    puts 'Email Sent! Message: #{bodyText} To: #{phoneNumber}'
 end
 
-sendEmail(messageSubject, targetAddress, username, password)
-
+sendEmail(bodyText, phoneNumber, username, password)
 
 def deleteMessage(keyword, username, password)
     
     Gmail.connect(username, password) do |gmail|
         gmail.inbox.emails(:unread, gm: keyword).each do |email| 
-            puts email.body
+            #puts email.body
             email.read!
             email.delete!
         end
-	end
+    end
+    puts "deleted unread with keyword: #{keyword}"
 end
 
 deleteMessage(withKeyword, username, password)
