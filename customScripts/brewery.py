b234a8724af0283270c36f00039657ef24cc969b
Izaac <izaac.kirscht@gmail.com>
2017-08-01 15:38:19 -0500

    Update brewery.py

--- customScripts/brewery.py
+++ customScripts/brewery.py
@@ -1,7 +1,7 @@
 from brewerydb.brewerydb import BreweryDB
 import json, sys, os
 
-with open('%s/customScripts/config.json' % os.getcwd(), 'r') as f:
+with open('%s/config.json' % os.getcwd(), 'r') as f:
     config = json.load(f)
 
 brewAPI = BreweryDB(config['breweryDB'])
