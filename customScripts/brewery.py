283edd92a1fa8fbaa5d6c611d6849f2ec3c73db6
Izaac <izaac.kirscht@gmail.com>
2017-08-01 11:58:57 -0500

    Update brewery.py

--- customScripts/brewery.py
+++ customScripts/brewery.py
@@ -12,7 +12,7 @@ try:
     website = beers.brewery["website"]
     breweryName = beers.brewery["name"]
 
-    daGoodz = "Brew: %s\nName: %s\nABV: %s\nIBU: %s\nStyle: %s\nWebsite: %s\nImage:%s" % (breweryName, beers.name, beers.abv, beers.ibu, beers.style[0:20], website, beers.label_image_medium)
+    daGoodz = "Brew: %s\nName: %s\nABV: %s\nIBU: %s\nStyle: %s\nWebsite: %s\nImage: %s" % (breweryName, beers.name, beers.abv, beers.ibu, beers.style[0:20], website, beers.label_image_medium)
     print daGoodz
 except:
     print "Beer not found."
