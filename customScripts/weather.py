310f77fa808f2e8df4238cd1996b8ef5806aa48d
Izaac <izaac.kirscht@gmail.com>
2017-08-01 15:39:04 -0500

    Update weather.py

--- customScripts/weather.py
+++ customScripts/weather.py
@@ -1,6 +1,6 @@
 import requests, json, time, sys, os
 
-with open('%s/customScripts/config.json' % os.getcwd(), 'r') as f:
+with open('%s/config.json' % os.getcwd(), 'r') as f:
 	config = json.load(f, strict=False)
 
 darkSkyKey = config['darkSkyKey']
